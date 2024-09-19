77504040622c950a907553deef6e08d3f73a0808
madhumitha102 <madhumitharengaraj@gmail.com>
2024-09-19 10:59:03 +0530

    added bootrap

--- src/component/ProductComp.jsx
+++ src/component/ProductComp.jsx
@@ -8,6 +8,19 @@ function ProductComp(){
     return(
         <div>
             <h1>product page</h1>
+
+            <div className="row">
+            {data.map((da)=>(
+                <div class="card col-3" style={{width: "18rem"}}>
+                <img src={da.image} class="card-img-top" alt="..."/>
+                <div class="card-body">
+                  <p class="card-text">{da.title}---{da.price}</p>
+                </div>
+              </div>
+            ))}
+            </div>
+            
+            
         </div>
     )
 }
